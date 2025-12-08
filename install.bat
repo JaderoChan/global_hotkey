ed4f882e5ad9b254c18210e3eadae36202ebf1df
JaderoChan <c_dl_cn@outlook.com>
2025-12-08 14:55:17 +0800

    Fix a bug

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
-cmake -B build -DGLOBAL_HOTKEY_BUILD_EXAMPLE=OFF
+cmake -B build -DGLOBAL_HOTKEY_BUILD_EXAMPLE=OFF -DGLOBAL_HOTKEY_BUILD_SHARED=ON
 make -C build -j
 cmake --install build --prefix=install
