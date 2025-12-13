4e5649e9499242839a60e3c03dfaa8709e2bf718
JaderoChan <c_dl_cn@outlook.com>
2025-12-14 02:20:44 +0800

    Update scripts

--- install.bat
+++ install.bat
@@ -1,3 +1,3 @@
 cmake -B build -DGLOBAL_HOTKEY_BUILD_EXAMPLE=OFF
 cmake --build build -j
-cmake --install build --prefix=install
+cmake --install build --prefix install
--- install_for_wayland.sh
+++ install_for_wayland.sh
@@ -1,3 +1,3 @@
 cmake -B build -DGLOBAL_HOTKEY_BUILD_EXAMPLE=OFF -DGLOBAL_HOTKEY_DISABLE_REGISTER=ON -DGLOBAL_HOTKEY_DISABLE_HOOK=OFF
 cmake --build build -j
-cmake --install build --prefix=install
+cmake --install build --prefix install
